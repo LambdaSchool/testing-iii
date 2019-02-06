29b9603b85639f4cf47b9590d32e6dd84d986005
Jason Hedrick <jasonleehedrick@gmail.com>
2019-02-05 18:30:49 -0800

    Filled out the Display.spec.js test items.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,26 @@
 // Test away!
+import React from 'react';
+import Display from './Display';
+import {render} from 'react-testing-library';
+
+describe('Display renders without crashing.', () => {
+    it('Renders the Display component', () => {
+        render(<Display closed={false} locked={false} />);
+    })
+    it('Displays open if the gate is open.', () => {
+        const { getByText } = render(<Display closed={false} locked={false} />);
+        getByText(/open/i);
+    })
+    it('Displays unlocked if the gate is unlocked', () => {
+        const { getByText } = render(<Display closed={false} locked={false} />);
+        getByText(/unlocked/i);
+    })
+    it('Displays closed if the gate is closed.', () => {
+        const { getByText } = render(<Display closed={true} locked={true} />);
+        getByText(/closed/i);
+    })
+    it('Displays locked if the gate is locked', () => {
+        const { getByText } = render(<Display closed={true} locked={true} />);
+        getByText(/locked/i);
+    })
+})
