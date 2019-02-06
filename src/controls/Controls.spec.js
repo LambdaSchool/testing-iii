6334b9a79b05e9faddb14cea07c32704c5a2cbae
jbrock1981 <jbrock1981@gmail.com>
2019-02-06 08:38:09 -0600

    boilerplated controls.spec.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls';
+import Dashboard from '../dashboard/Dashboard';
+
+afterEach(cleanup);
+
+describe('The Controls Component', () => {
+    
+})
