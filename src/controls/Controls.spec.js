7915396e17e7bfe1c2a0fb2ce012c4c4b0d2e112
Jason Hedrick <jasonleehedrick@gmail.com>
2019-02-05 20:56:20 -0800

    Added an extra describe in Controls.spec.js and Display.spec.js to explain testing changes based on logic.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -8,11 +8,14 @@ afterEach(cleanup);
 describe('Controls renders without crashing.', () => {
     it('Renders the Controls component', () => {
         render(<Controls />)
-    })
+    });
+});
+
+describe('Testing the elements in Controls change based on different logic.', () => {
     it('Displays Lock Gate if the gate is unlocked', () => {
         const { getByText } = render(<Controls locked={false} closed={false} />);
         getByText(/lock gate/i);
-    })
+    });
     it('Displays Close Gate if the gate is open', () => {
         const { getByText } = render(<Controls locked={false} closed={false} />);
         getByText(/close gate/i);
@@ -20,18 +23,17 @@ describe('Controls renders without crashing.', () => {
     it('Displays Unlock Gate if the gate is locked', () => {
         const { getByText } = render(<Controls locked={true} closed={true} />);
         getByText(/unlock gate/i);
-    })
+    });
     it('Displays Open Gate if the gate is closed', () => {
         const { getByText } = render(<Controls locked={true} closed={true} />);
         getByText(/open gate/i);
-    })
+    });
     it('Will disable the Lock Gate button if the gate is open', () => {
         const { getByText } = render(<Controls locked={false} closed={false} />)
         expect(getByText(/lock gate/i).disabled).toBe(true);
-    })
+    });
     it('Will disable the Open Gate button if the gate is locked', () => {
         const { getByText } = render(<Controls locked={true} closed={true} />);
         expect(getByText(/open gate/i).disabled).toBe(true);
-    })
-
+    });
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -8,31 +8,34 @@ afterEach(cleanup);
 describe('Display renders without crashing.', () => {
     it('Renders the Display component', () => {
         render(<Display closed={false} locked={false} />);
-    })
+    });
+});
+
+describe('Testing the elements in Display change based on different logic ', () => {
     it('Displays open if the gate is open.', () => {
         const { getByText } = render(<Display closed={false} locked={false} />);
         getByText(/open/i);
-    })
+    });
     it('Displays unlocked if the gate is unlocked', () => {
         const { getByText } = render(<Display closed={false} locked={false} />);
         getByText(/unlocked/i);
-    })
+    });
     it('Uses the green-led class when the gate is unlocked and opened.', () => {
         const { getByText } = render(<Display closed={false} locked={false} />);
         expect(getByText(/open/i).className.includes('green-led')).toBe(true);
         expect(getByText(/unlocked/i).className.includes('green-led')).toBe(true);
-    })
+    });
     it('Displays closed if the gate is closed.', () => {
         const { getByText } = render(<Display closed={true} locked={true} />);
         getByText(/closed/i);
-    })
+    });
     it('Displays locked if the gate is locked', () => {
         const { getByText } = render(<Display closed={true} locked={true} />);
         getByText(/locked/i);
-    })
+    });
     it('Uses the red-led class when the gate is locked and closed.', () => {
         const { getByText } = render(<Display closed={true} locked={true} />);
         expect(getByText(/closed/i).className.includes('red-led')).toBe(true);
         expect(getByText(/locked/i).className.includes('red-led')).toBe(true);
-    })
-})
+    });
+});
