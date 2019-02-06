71c76193c513262c2b63c548f2772a563eeece6a
Joseph Thompson <joe.thompson838@gmail.com>
2019-02-06 18:59:17 -0500

    added tests for dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+import React from "react";
+import { render, cleanup } from "react-testing-library";
+
+import Dashboard from "../dashboard/Dashboard";
+import Display from "../display/Display";
+
+describe("Dashboard component", () => {
+  afterEach(cleanup);
+
+  it("renders the dashboard component", () => {
+    render(<Dashboard />);
+  });
+
+  it("dashboard renders a Display component", () => {
+    render(<Display />);
+  });
+});
