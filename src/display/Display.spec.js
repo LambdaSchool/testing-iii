4fb1805869a76e5e2da34ac25b19694622acaac0
jbrock1981 <jbrock1981@gmail.com>
2019-02-06 08:43:20 -0600

    boilerplated display.spec.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+
+import Display from './Display';
+
+afterEach(cleanup);
+
+describe('Display renders', () => {
+    
+})
