df485cb29a58b738f69944c94ca2a5258f5756ca
Jason Hedrick <jasonleehedrick@gmail.com>
2019-02-05 18:43:46 -0800

    Setup the Dashboard.spec.js test file.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,8 @@
 // Test away!
+import React from 'react';
+import Controls from './Controls';
+import {render} from 'react-testing-library';
+
+describe('Controls renders without crashing.', () => {
+
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+import React from 'react';
+import Dashboard from './Dashboard';
+import {render} from 'react-testing-library';
+
+describe('Dashboard renders without crashing.', () => {
+    it('Renders the Dashboard component', () => {
+        render(<Dashboard />);
+    })
+    it('Renders the Display component', () => {
+        const { getByText } = render(<Dashboard />);
+        getByText(/open/i);
+        getByText(/unlocked/i);
+    })
+    it('Renders the Controls component', () => {
+        const { getByText } = render(<Dashboard />);
+        getByText(/close gate/i);
+        getByText(/lock gate/i);
+    })
+});
