ce1244cc2ba4e6a000fd073cd89fbe4b1f4b407f
jbrock1981 <jbrock1981@gmail.com>
2019-02-06 13:31:07 -0600

    Controls.spec.js completed, testing successfully

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -9,5 +9,24 @@ import Dashboard from '../dashboard/Dashboard';
 afterEach(cleanup);
 
 describe('The Controls Component', () => {
+    test('renders the Control component', () => {
+        render(<Controls />)
+    })
+
+    test('has toggle buttons', () => {
+        const { getByText } = render(<Controls closed={false} locked={true} />);
+        getByText(/lock gate/i);
+        getByText(/close gate/i);
+    })
     
-})
+    test('cant disable when open and unlocked', () => {
+        const { getByText } = render(<Controls closed={false} locked={false} />);
+        const toggleLockButton = getByText(/lock gate/i);
+        expect(toggleLockButton).toHaveAttribute('disabled', '');
+    })
+
+    test('cant close when already closed', () => {
+        const { getByText } = render(<Controls closed={true} />);
+        getByText(/open gate/i);
+    })
+});
