1b3b818a8a8bc686c73402973304de3b5e2e7a16
vy3191 <venkyafhs@gmail.com>
2019-02-06 18:56:32 -0500

    Initial commit Dependencies added Project set up done

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
 // Test away!
+import React from 'react';
+import {render, clickEvent} from 'react-testing-library';
+import Display from './Display';
+
+describe('Tests for Display Component', () => {
+      test('It should display the display component', () => {
+            const {getByText, getByTestId} = render(<Display />);
+              
+      });
+
+});
