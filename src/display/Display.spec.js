3db781f9f03ade733d450cc0024d5e4d0ae92361
Joseph Thompson <joe.thompson838@gmail.com>
2019-02-06 20:00:00 -0500

    display testing completed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,48 @@
 // Test away!
+import React from "react";
+import { render, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+
+import Display from "./Display.js";
+import Dashboard from "../dashboard/Dashboard";
+
+describe("The Display Component", () => {
+  afterEach(cleanup);
+  test("green-led on start", () => {
+    const component = render(<Display className="green-led" />);
+    const closed = component.getByTestId("unlocked");
+    expect(closed).toHaveClass("green-led");
+  });
+
+  test("unlocked on start", () => {
+    const component = render(<Display className="green-led" />);
+    const locked = component.getByTestId("closed");
+    expect(locked).toHaveClass("green-led");
+  });
+
+  test("red-led and closed", () => {
+    const component = render(<Display closed={true} />);
+    const closed = component.getByTestId("closed");
+    expect(closed).toHaveTextContent(/closed/i);
+    expect(closed).toHaveClass("red-led");
+  });
+  test("green-led and open", () => {
+    const component = render(<Dashboard closed={false} />);
+    const closed = component.getByTestId("closed");
+    expect(closed).toHaveTextContent(/open/i);
+    expect(closed).toHaveClass("green-led");
+  });
+  test("locked text visible", () => {
+    const component = render(<Display locked={true} />);
+    const locked = component.getByTestId("unlocked");
+    expect(locked).toHaveTextContent(/locked/i);
+    expect(locked).toHaveClass("red-led");
+  });
+  test("unlocked text visible", () => {
+    const component = render(<Display locked={false} />);
+    const locked = component.getByTestId("unlocked");
+    expect(locked).toHaveTextContent(/unlocked/i);
+    expect(locked).toHaveClass("green-led");
+  });
+});
