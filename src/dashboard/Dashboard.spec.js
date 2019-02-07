6afbc38f06dcb1573ee58bf1065ab769ab5386ea
vy3191 <venkyafhs@gmail.com>
2019-02-06 20:20:24 -0500

    Completed dashboard.spec.tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+import React from 'react';
+import {render} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Display from '../display/Display';
+import Controls from '../controls/Controls';
+
+describe('Testing Dashboard Component', () => {
+     test('It should display The Display Component', ()=> {
+          render(<Display />);
+     });
+
+     test('It should display Controls Component', () => {
+        render(<Controls />);
+     });
+});
