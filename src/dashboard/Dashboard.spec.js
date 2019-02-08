ccf63d18c560338a2980f234cdb32e1a1a905fcb
Matthew Meitl <darksamus6@gmail.com>
2019-02-07 19:27:43 -0600

    added tests for controls, display, and dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dashboard from './Dashboard';
+
+ describe('<Dashboard/>', () => {
+   it('should render display and controls', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<Dashboard />, div);
+      ReactDOM.unmountComponentAtNode(div);
+   });
+});
