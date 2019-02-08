fc6c1c0edb0462909a061d34e4b71762d953aad6
Chris Roth <c.roth.lambda@gmail.com>
2019-02-08 15:03:47 -0600

    Testing-iii: installed yarn and started basic testing boilerplate for display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
 // Test away!
+import React from 'react';
+import 'jest-dom/extend-expect';
+import { render } from 'react-testing-library';
+import Display from './Display';
+
+describe('display component tests', ()=>{
+    it('Renders Display Component',()=>{
+        render(<Display />);
+    }),
+    it()
+})
