ef2e50f48dc8d238915492970abd161366ab6128
Freekeyboy <jjhutton81@gmail.com>
2019-02-07 19:40:29 -0800

    Basic outline for components tests.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,3 +2,11 @@
 import React from "react";
 import { render, fireEvent } from "react-testing-library";
 import "jest-dom/extend-expect"; 
+
+import Controls from "./Controls";
+
+describe("CONTROLS", () => {
+  it("Render buttons to toggle.", () => {});
+
+  it("Close gate button becomes open gate button when clicked.", () => {});
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,3 +2,9 @@
 import React from "react";
 import { render, fireEvent } from "react-testing-library";
 import "jest-dom/extend-expect";
+
+import Dashboard from "./Dashboard";
+
+describe("DASHBOARD", () => {
+    it("Render Display and Controls", () => {});
+});
