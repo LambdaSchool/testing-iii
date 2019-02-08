a863cdff080cab87ea22b23fde4157a729b388f2
shannonhealy <sahealy525@gmail.com>
2019-02-07 21:51:58 -0500

    fixed one of two misbehaving tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -7,15 +7,15 @@ import Controls from "./Controls";
 describe("the lock button", () => {
     it("is disabled while gate is open", () => {
         const component = render(<Controls closed={false} locked={false} />);
-        const button = component.getByText("Lock Gate");
+        const button = component.getByText("Lock Gate"); 
         
         expect(button).toBeDisabled();
     });
     it("is enabled while gate is closed", () => {
-        const component = render(<Controls closed={true} locked={false} />);
-        const button = component.getByText("Lock Gate");
+        const component = render(<Controls closed={true} locked={true} />);
+        const button = component.getByText("Unlock Gate");
         
-        expect(button).not.toBeDisabled();
+        expect(button).not.toHaveAttribute("disabled");
         //test is seeing button as disabled even when closed=true, despite working as expected in the browser
     });
     it("says 'lock' while gate is unlocked", () => {
@@ -35,7 +35,7 @@ describe("the open button", () => {
         const component = render(<Controls closed={true} locked={true} />);
         const button = component.getByText("Open Gate");
         
-        expect(button).toBeDisabled();
+        expect(button).toHaveAttribute("disabled"); 
         //test is seeing button as enabled even when locked=true, despite working as expected in the browser
     });
     it("is enabled while gate is unlocked", () => {
