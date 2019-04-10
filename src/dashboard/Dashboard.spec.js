207676291128b5486126ec272c88c9c9f867f7db
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-10 14:19:31 -0700

    Scaffold Dashboard component test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,6 @@
-// Test away
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each';
+
+describe('Dashboard component tests', () => {});
