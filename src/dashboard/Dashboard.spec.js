4c7723f5f24ab4a6b66058d957324d024a93e9cd
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-10 14:23:24 -0700

    Create Dashboard component tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
-import renderer from 'react-test-renderer';
+import { render } from 'react-testing-library';
 import 'react-testing-library/cleanup-after-each';
+import Dashboard from './Dashboard';
 
-describe('Dashboard component tests', () => {});
+describe('Dashboard component tests', () => {
+	test('should render controls and display', () => {
+		const { getByText } = render(<Dashboard />);
+
+		getByText(/unlocked/i);
+		getByText(/open/i);
+		getByText(/lock gate/i);
+		getByText(/close gate/i);
+	});
+});
