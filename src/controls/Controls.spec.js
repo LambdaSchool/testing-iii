5d00acaf1a71534f72cf9d6410f3658a953aebe9
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-10 14:29:32 -0700

    Scaffold Control component test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,8 @@
-// Test away!
+import React from 'react';
+import { toBeDisabled } from 'jest-dom';
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+
+expect.extend({ toBeDisabled });
+
+describe('Control component tests', () => {});
