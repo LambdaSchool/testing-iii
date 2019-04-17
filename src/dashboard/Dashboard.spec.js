17bf5dd774a132b097b8f5264b57467b362e3a31
Kseniya Platonava <ksu2008@gmail.com>
2019-04-16 22:45:36 -0400

    Add Dashboard That Shows The onstrols and Display:

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
-// Test away
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
+import { render, fireEvent } from "react-testing-library";
+
+ import Dashboard from "./Dashboard";
+describe("<Dashboard/>", () => {
+  it("renders test", () => {
+ 
+    render(<Dashboard />);
+  });
+  it("snapshot match", () => {
+    const tree = renderer.create(<Dashboard />); 
+  
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
