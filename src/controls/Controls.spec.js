d542dad39c09ef3053d02571ac2e4ed292be4a21
Cody Brewer <brewer.cody.m@gmail.com>
2019-06-25 20:20:39 -0600

    removed npm lock, controls test done

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Controls from './Controls';
+
+describe('<Control />', () => {
+    it('should match snapshot', () => {
+        const tree = renderer.create(<Controls />).toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+})
