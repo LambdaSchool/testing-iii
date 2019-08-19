a88caaf0733fd4c5bb32870de3a6423cbcb332a6
Greg Johnston <GCJohnston91@gmail.com>
2019-08-19 18:08:04 -0400

    Updated Dashboard to queryByText

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -13,30 +13,29 @@ describe('<Dashboard />', () => {
       expect(getByText('Close Gate'));
     })
     it('has buttons that change state', () => {
-      const { getByText } = render(<Dashboard />);
-
-      const closeButton = getByText("Close Gate")
+      const { getByText, queryByText } = render(<Dashboard />);
+      const closeButton = queryByText("Close Gate")
       fireEvent.click(closeButton);
       expect(getByText('Unlocked'));
       expect(getByText('Closed'));
       expect(getByText('Lock Gate'));
       expect(getByText('Open Gate'));
 
-      const lockButton = getByText("Lock Gate")
+      const lockButton = queryByText("Lock Gate")
       fireEvent.click(lockButton)
       expect(getByText('Locked'));
       expect(getByText('Closed'));
       expect(getByText('Unlock Gate'));
       expect(getByText('Open Gate'));
 
-      const unlockButton = getByText("Unlock Gate")
+      const unlockButton = queryByText("Unlock Gate")
       fireEvent.click(lockButton)
       expect(getByText('Unlocked'));
       expect(getByText('Closed'));
       expect(getByText('Lock Gate'));
       expect(getByText('Open Gate'));
 
-      const openButton = getByText("Open Gate")
+      const openButton = queryByText("Open Gate")
       fireEvent.click(openButton)
       expect(getByText('Unlocked'));
       expect(getByText('Open'));
