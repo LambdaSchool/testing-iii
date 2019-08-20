5faf8ed8d8cda83002b716e2b77fd75bfd9ea8a1
Carla Marvin <carlamarvin78@gmail.com>
2019-08-20 17:38:03 -0400

    Dashboard test complete

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,34 @@
 // Test away
+
+import React from "react";
+import renderer from 'react-test-renderer';
+import Dashboard from './Dashboard';
+import { render, fireEvent } from "@testing-library/react";
+
+describe('Dashboard />', () => {
+    it('should match snapshot', () => {
+      const tree = renderer.create(<Dashboard />).toJSON();
+  
+      expect(tree).toMatchSnapshot();
+    });
+    it('defaults to open and unlocked', () => {
+      const { getByText } = render(<Dashboard />);
+      getByText(/Open/);
+      getByText(/Unlocked/);
+    });
+    it('cannot be open/closed when locked', () => {
+      const { getByText } = render(<Dashboard />);
+      const lockButton = getByText(/Lock Gate/);
+      const closeButton = getByText(/Close Gate/);
+      fireEvent.click(closeButton);
+      fireEvent.click(lockButton);
+      expect(closeButton.disabled).toBe(true); //open Button disabled
+    });
+    it('shows the controls and display', () => {
+      const {getByText} = render(<Dashboard />);
+      getByText(/Open/);
+      getByText(/Unlocked/);
+      getByText(/Lock Gate/);
+      getByText(/Close Gate/);
+    });
+  });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+
+import React from "react";
+import renderer from 'react-test-renderer';
+import Display from './Display';
+import { render, fireEvent } from "@testing-library/react";
+
+describe('Display />', () => {
+    it('should match snapshot', () => {
+      const tree = renderer.create(<Display />).toJSON();
+  
+      expect(tree).toMatchSnapshot();
+    });
+  });
