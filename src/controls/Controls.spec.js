a005d28fe861743f68bd38bd44da1ab267c777fb
Carla Marvin <carlamarvin78@gmail.com>
2019-08-20 23:51:26 -0400

    Display tests completed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -3,11 +3,14 @@
 import React from "react";
 import renderer from 'react-test-renderer';
 import Controls from './Controls';
+import { render, fireEvent } from "@testing-library/react";
 
 describe('Controls />', () => {
     it('should match snapshot', () => {
       const tree = renderer.create(<Controls />).toJSON();
-  
       expect(tree).toMatchSnapshot();
+    });
+    it('provides buttons to toggle the closed and locked states', () => {
+      const { getByText } = render(<Controls />);
     });
   });
