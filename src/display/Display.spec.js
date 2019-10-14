77b6c47768f7735287835685ce4b26de3b72b373
Oluwajoba <oluwajobabello@gmail.com>
2019-10-14 14:15:48 +0100

    displays Unlocked if gate is Locked

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -13,7 +13,6 @@ describe('<Display />', () => {
     let wrapper1 = rtl.render(<Display />);
     let wrapper2 = rtl.render(<Controls />);
   it("should displays Unlocked if gate is Locked", () => {
-    
     expect(wrapper1.queryByText(/Unlocked/i)).toBeInTheDocument();
     // expect(wrapper.queryByText(/Closed/i)).toBeInTheDocument();
     rtl.fireEvent.click(wrapper2.queryByText(/Lock Gate/i));
