93190fe6dee6dd7c75f71d363c7466a32f727ca9
Oluwajoba <oluwajobabello@gmail.com>
2019-10-14 20:01:19 +0100

    when unlocked or open use the green-led class / Display.spec.js / Dispaly.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -6,6 +6,7 @@ import React from "react";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
+
 import Display from './Display/';
 import Controls from "../controls/Controls";
 
@@ -21,20 +22,33 @@ describe('<Display />', () => {
     });
 
     it("should displays 'Closed' if the closed prop is true", () => {
-        let wrapper1 = rtl.render(<Display closed={true}/>);
+        let wrapper1 = rtl.render(<Display closed={true} />);
         expect(wrapper1.queryByText(/Closed/i)).toBeInTheDocument();
     });
     it("should displays 'Open' if the closed prop is false", () => {
-        let wrapper1 = rtl.render(<Display closed={false}/>);
+        let wrapper1 = rtl.render(<Display closed={false} />);
         expect(wrapper1.queryByText(/Open/i)).toBeInTheDocument();
     });
     it("should displays 'Locked' if the locked prop is true", () => {
-        let wrapper1 = rtl.render(<Display locked={true}/>);
+        let wrapper1 = rtl.render(<Display locked={true} />);
         expect(wrapper1.queryByText(/Locked/i)).toBeInTheDocument();
     });
     it("should displays 'Unlocked' if the locked prop is false", () => {
-        let wrapper1 = rtl.render(<Display locked={false}/>);
+        let wrapper1 = rtl.render(<Display locked={false} />);
         expect(wrapper1.queryByText(/Locked/i)).toBeInTheDocument();
     });
-    
+    it("when unlocked or open use the green-led class", () => {
+        let wrapper1 = rtl.render(<Display />);
+        let expected = wrapper1.container.firstChild.firstChild.childNodes[0].textContent;
+        let actual = wrapper1.container.firstChild.firstChild.classList[1];
+        // console.log("aaaa: ", wrapper1.container.firstChild.firstChild.classList[1]);
+        // console.log("bbbb: ", wrapper1.container.firstChild.firstChild.childNodes[0].textContent)
+        expect(actual).toEqual("green-led");
+        expect(expected).toEqual("Unlocked");
+       
+        
+
+    });
+
+
 });
