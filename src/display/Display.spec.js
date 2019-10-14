7226f3b4aaccc98606a17813d07b71dc4ace1af4
Oluwajoba <oluwajobabello@gmail.com>
2019-10-15 00:05:35 +0100

    code clean-up2 / complete MVP

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -9,6 +9,7 @@ import Controls from "../controls/Controls";
 describe('<Display />', () => {
 
     it("should display if gate is open/closed and if it is locked/unlocked", () => {
+
         let wrapper1 = rtl.render(<Display />);
         let wrapper2 = rtl.render(<Controls />);
         expect(wrapper1.queryByText(/Unlocked/i)).toBeInTheDocument();
@@ -17,26 +18,31 @@ describe('<Display />', () => {
     });
 
     it("should displays 'Closed' if the closed prop is true", () => {
+
         let wrapper1 = rtl.render(<Display closed={true} />);
         expect(wrapper1.queryByText(/Closed/i)).toBeInTheDocument();
     });
 
     it("should displays 'Open' if the closed prop is false", () => {
+
         let wrapper1 = rtl.render(<Display closed={false} />);
         expect(wrapper1.queryByText(/Open/i)).toBeInTheDocument();
     });
 
     it("should displays 'Locked' if the locked prop is true", () => {
+
         let wrapper1 = rtl.render(<Display locked={true} />);
         expect(wrapper1.queryByText(/Locked/i)).toBeInTheDocument();
     });
 
     it("should displays 'Unlocked' if the locked prop is false", () => {
+
         let wrapper1 = rtl.render(<Display locked={false} />);
         expect(wrapper1.queryByText(/Locked/i)).toBeInTheDocument();
     });
-    
+
     it("when unlocked or open use the green-led class", () => {
+        
         let wrapper1 = rtl.render(<Display />);
         let expected = wrapper1.container.firstChild.firstChild.childNodes[0].textContent;
         let actual = wrapper1.container.firstChild.firstChild.classList[1];
@@ -45,6 +51,7 @@ describe('<Display />', () => {
     });
 
     it("when locked or closed use the red-led class", () => {
+        
         let wrapper1 = rtl.render(<Display />);
         let expected = wrapper1.container.firstChild.firstChild.childNodes[0].textContent;
         let actual = wrapper1.container.firstChild.firstChild.classList[0];
