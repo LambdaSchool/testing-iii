4a640d0912719b041336f56c3cbf6cc185c3b7e2
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-10-14 20:04:03 -0400

    initial commit and partially compelte controls test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent, wait } from 'react-testing-library';
+import Controls from './Controls';
+
+
+describe("Controls Test", () => {
+    it("Button opens while unlocked", () => {
+        const mock = jest.fn();
+        const { queryByText } = render(<Controls locked={false} closed={false} toggleClosed={mock} />)
+        const lockButton = queryByText("Lock Gate");
+        expect(lockButton.disabled).toBe(true);
+        const closeButton = queryByText("Close Gate")
+        expect(closeButton.disabled).toBe(false);
+        fireEvent.click(closeButton);
+        expect(mock).toBeCalled()
+    }) 
+})
