17375580857f4e5781f353cb8225799050e5bfb6
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-10-14 20:36:43 -0400

    Dashboard testing finished

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import Dashboard from './Dashboard';
+import Controls from '../controls/Controls'
+
+
+describe("Dashboard Testing", () => {
+    it('Default control and display is rendering', () =>{
+        const { getByText } = render(<Dashboard />);
+        //Unlocked Open Lock Gate Close Gate
+        expect(getByText("Unlocked"));
+        expect(getByText("Open"));
+        expect(getByText("Lock Gate"));
+        expect(getByText("Close Gate"));        
+    });
+})
