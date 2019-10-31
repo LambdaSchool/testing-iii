4d8e992fe7cbcbb9e027dd6e41109777f333688c
spettigrew <sara.pettigrew5@gmail.com>
2019-10-31 14:39:57 -0600

    set up other testing modules - passed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,32 +3,64 @@ import React from "react";
 import  Dashboard from './Dashboard';
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 afterEach(rtl.cleanup);
 
-test('Is locked', async () => {
-    const wrapper = rtl.render(<Dashboard /> );
-    await wrapper.findAllByText(/locked/i);
-
-    const locked = wrapper.getByText(/locked/i)
-
-    rtl.act(() => {
-        rtl.fireEvent.click(locked)
-    })
-
-    expect(wrapper.queryByText(/locked/i)).toBeNull()
+test('Renders Dashboard/display correctly', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
 })
 
-test('Is closed', async () => {
-    const wrapper = rtl.render(<Dashboard />);
-    await wrapper.findAllByText(/closed/i);
+test('Close on click of close gate', async () => {
+    const { getByText, getByTestId } = rtl.render(<Dashboard />);
 
-    const closed = wrapper.getByText(/closed/i)
+    const closedMessage = getByTestId('gate-feature');
+    expect(closedMessage).not.toBeNull();
 
-    rtl.act(() => {
-        rtl.fireEvent.click(closed)
-    })
+    const button = getByTestId('gate-feature');
+    await fireEvent.click(button);
 
-    expect(wrapper.queryByText(/closed/i)).toBeNull()
+    expect(getByText(/closed/i));
+});
+
+test('Locked when locking gate after closing gate', async () => {
+    const { getByText, getByTestId } = rtl.render(<Dashboard />);
+    const toggleGate = getByTestId('gate-feature');
+    const lockGate = getByTestId('locking-feature');
+
+    const toggleGateButton= toggleGate;
+    const lockGateButton = lockGate;
+
+    fireEvent.click(toggleGateButton);
+    expect(getBy(/closed/i));
+    await fireEvent.click(lockGateButton);
+    expect(getByText(/locked/i));
 })
+
+// test('Is locked', async () => {
+//     const wrapper = rtl.render(<Dashboard /> );
+//     await wrapper.findAllByText(/locked/i);
+
+//     const locked = wrapper.getByText(/locked/i)
+
+//     rtl.act(() => {
+//         rtl.fireEvent.click(locked)
+//     })
+
+//     expect(wrapper.queryByText(/locked/i)).toBeNull()
+// })
+
+// test('Is closed', async () => {
+//     const wrapper = rtl.render(<Dashboard />);
+//     await wrapper.findAllByText(/closed/i);
+
+//     const closed = wrapper.getByText(/closed/i)
+
+//     rtl.act(() => {
+//         rtl.fireEvent.click(closed)
+//     })
+
+//     expect(wrapper.queryByText(/closed/i)).toBeNull()
+// })
 
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,5 +2,15 @@
 import React from "react";
 import Display from './Display'
 
-import * as rtl from "@testing-library/react";
+// import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+
+
+afterEach(rtl.cleanup);
+
+test("Render Display", () => {
+    expect(render(<Display />)).toMatchSnapshot();
+})
+
+test('')
