a4af3d5663f4f502913e83218a7138ba58758a6d
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-11-11 14:40:45 -0600

    dashboard snapshot created

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
-// Test away
+import React from 'react';
+import { render } from 'react-testing-library';
+
+// Test away!
+
+import Dashboard from './Dashboard';
+
+test ('Display renders correctly', () => {
+    expect(render(<Dashboard/>)).toMatchSnapshot();
+});
+
