89732cdbe3184473e4025154c3ad277f1ea36951
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-11-11 14:39:17 -0600

    display snapshot created

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+
 // Test away!
+
+import Display from './Display';
+
+test ('Display renders correctly', () => {
+    expect(render(<Display/>)).toMatchSnapshot();
+});
+
