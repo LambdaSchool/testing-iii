d353d9f364da2a1f84cb639dd0e1d37867e7277b
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-11 15:27:14 -0500

    starting, setup all done

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,10 @@
 // Test away!
+
+
+test('renders "success" text', () => {
+    const { getByText, findByText } = render(<App />);
+    // Click button
+    fireEvent.click(getByText("Get message!"));
+    findByText(/success/i);
+    //Unlock Gate
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
 // Test away
+
+
+it('renders "success" text', () => {
+    const { getByText, findByText } = render(<App />);
+    // Click button
+    fireEvent.click(getByText("Get message!"));
+    findByText(/success/i);
+});
