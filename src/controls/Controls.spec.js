3e54d8f36a326f0a7ffa95de33002c5de36c78b0
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-11-11 14:41:35 -0600

    controls snapshot created

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,3 +1,11 @@
-
+import React from 'react';
+import { render } from 'react-testing-library';
 
 // Test away!
+
+import Controls from './Controls';
+
+test ('Display renders correctly', () => {
+    expect(render(<Controls/>)).toMatchSnapshot();
+});
+
