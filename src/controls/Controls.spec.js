ed80fc10a6eaaf3198fc9923600df9505ffdba08
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-13 18:30:18 -0500

    snapshot for controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Controls from "./Controls";
+
+test("Controls renders correctly", () => {
+    expect(render(<Controls/>)).toMatchSnapshot();
+});
+
+test("")
