81ad7b4e5d7bd4e3544442612bed5ce4eb6f2e23
Toniddarden1 <Toniddarden@gmail.com>
2019-12-04 22:56:32 -0500

    added if/else test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
 // Test away
+// testing control/dashboard panel display 
+
+
+test("Render Parent Snapshot", async () => {
+    const wrapper = rtl.render(<Dashboard />);
+   expect(wrapper).toBeDefined();
+   expect(wrapper.asFragment()).toMatchSnapshot();
+
+})
