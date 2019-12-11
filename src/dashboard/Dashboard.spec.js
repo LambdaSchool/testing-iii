52a60970649dc4b893dc3407e39c0d6c18288eb9
Veto Ramirez <vetorobert3@gmail.com>
2019-12-10 23:24:25 -0800

    working on tests for the dashoard. Think I need to do this technique for the display.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-libray/jest-dom/extend-expect';
+import Dashboard from 'Dashboard';
+import Controls from './Controls';
+
+test("Render conponents Dashboard and Controls", () => {
+  const wrapper = rtl.render(<Dashboard />);
+  const 
+})
