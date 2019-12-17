0eaf2ca0c6587fa20f25bfbf876d788f73a4480b
Young Won <youngw417@gmail.com>
2019-12-16 19:30:47 -0800

    1st commit

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,23 @@
 // Test away!
+
+import React from 'react';
+import Controls from './Controls';
+
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+
+
+test('click event renders correct items', () => {
+
+    const locked = true;
+    const closed = true; 
+    const { getByText} = render(<Controls locked={locked} closed={closed} />);
+    const unlock = getByText(/Unlocked/i);
+
+   fireEvent.click(getByText('Unlock Gate'));
+    expect(unlock).toHaveClass('green-led');
+    
+    
+
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
 // Test away!
+
+import React from 'react';
+import Display from './Display';
+import { render } from '@testing-library/react';
+
+test('Correct item is rendering based on closed and locked', () => {
+    const closed = true;
+    const locked = false;
+    const { getByText, rerender } = render(<Display closed={closed} locked={locked} />);
+    getByText('Unlocked');
+
+    rerender(<Display closed={closed} locked={locked} />)
+    getByText('Closed');
+
+
+})
