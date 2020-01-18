c049f463cb79eaaf7e7de9ffdb385e2705b45ba2
CLYPR Technologies <cbanks@clypr.co>
2020-01-18 14:20:33 -0500

    started testing in Controls, Display, and Dashboard

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,7 @@
-// Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+import Controls from './Controls';
+
+test('Display renders without crashing', () => {
+    render(<Controls />);
+});
